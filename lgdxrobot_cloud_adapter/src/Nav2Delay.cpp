79525117d54886231c843023a1b32aa272d0a5af
Kai Tung Yu <sam@bristolgram.uk>
2026-06-20 10:57:25 +0100

    update delay node

--- lgdxrobot_cloud_adapter/src/Nav2Delay.cpp
+++ lgdxrobot_cloud_adapter/src/Nav2Delay.cpp
@@ -4,33 +4,29 @@
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::Node::SharedPtr g_node = nullptr;
-  g_node = rclcpp::Node::make_shared("nav2_delay");
+  rclcpp::Node::SharedPtr node = rclcpp::Node::make_shared("nav2_delay");
 
-  g_node->declare_parameter("nav2_delay_enable", false);
-  bool delay = g_node->get_parameter("nav2_delay_enable").as_bool();
+  node->declare_parameter("nav2_delay_enable", false);
+  bool delay = node->get_parameter("nav2_delay_enable").as_bool();
   if (delay)
   {
-    RCLCPP_INFO(g_node->get_logger(), "Delay Nav2 until the cloud adapter is ready.");
+    RCLCPP_INFO(node->get_logger(), "Delay Nav2 until the cloud adapter is ready.");
   }
   else
   {
-    RCLCPP_INFO(g_node->get_logger(), "Do not delay Nav2.");
+    RCLCPP_INFO(node->get_logger(), "Start Nav2 immediately.");
     rclcpp::shutdown();
-    g_node = nullptr;
     return 0;
   }
 
-  auto server = g_node->create_service<std_srvs::srv::Empty>("cloud/nav2_delay",
-    [g_node](const std::shared_ptr<std_srvs::srv::Empty::Request>,
+  auto server = node->create_service<std_srvs::srv::Empty>("cloud/nav2_delay",
+    [node](const std::shared_ptr<std_srvs::srv::Empty::Request>,
       std::shared_ptr<std_srvs::srv::Empty::Response>)
     {
-      RCLCPP_INFO(g_node->get_logger(), "Start Nav2");
+      RCLCPP_INFO(node->get_logger(), "Starting Nav2.");
       rclcpp::shutdown();
     });
 
-  rclcpp::spin(g_node);
-  rclcpp::shutdown();
-  g_node = nullptr;
+  rclcpp::spin(node);
   return 0;
 }
